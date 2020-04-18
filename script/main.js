6b80163ef36e464a4be9ffee106e96e898ea6115
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-18 15:57:12 +0200

    add js Done

--- script/main.js
+++ script/main.js
@@ -13,17 +13,58 @@ $(document).ready(function () {
      * Usate un template nell’html e clone() per l’ inserimento del messaggio da fare in JS
      ************************************************************************************************/
 
-    // refs
-        
+    // refs   
     var inputSend = $('.bottom-bar-dx input'); //input
     var iconChange = $('.bottom-bar-dx i:last-child'); // Icona
 
+
+    // icon change on input focus
     inputSend.focus( function() {
         iconChange.removeClass('fas fa-microphone');
         iconChange.addClass('fas fa-paper-plane');
     });
 
+    // send message with click icon
+    $('#app').on('click', '.bottom-bar-dx i:last-child', function() {
 
+        // clono il template
+        var messageTemplates = $('.templates .send').clone(); //refs li clone
+
+        var sendMessage = inputSend.val().trim();
+        console.log(sendMessage);
+
+        if (sendMessage !== '') {
+            // send message add to clone
+            messageTemplates.children().children('h5').text(sendMessage);
+            // append to html chat
+            $('.message-content ul').append(messageTemplates);
+            // reset input
+            inputSend.val('');
+        }   
+    } );
+
+
+    inputSend.keyup(function (e) { 
+        // clono il template
+        var messageTemplates = $('.templates .send').clone(); //refs li clone
+
+        var sendMessage = inputSend.val().trim();
+        console.log(sendMessage);
+
+        if (sendMessage !== '' && e.which == 13 ) {
+             // send message add to clone
+             messageTemplates.children().children('h5').text(sendMessage);
+             // append to html chat
+             $('.message-content ul').append(messageTemplates);
+             // reset input
+             inputSend.val('');
+        }
+
+    });
+   
+
+    
+   
 
 
 
