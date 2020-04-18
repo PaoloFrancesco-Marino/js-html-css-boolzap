cbb7d5de32bf4d4a4cfc2eb8c2a130323a4f5346
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-18 12:26:47 +0200

    fix scroll-bar and add initial input.focus js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,31 @@
-console.log('js work');
+/**
+ * BoolaZap
+ */
+
+
+$(document).ready(function () {
+
+    /**
+     * Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando icona ‘invia il 
+     * testo’ viene aggiunto al thread sopra, come messaggio verde (ricordate focus() )
+     * Font family: Lato
+     * Messaggi visibili inizialmente sono inseriti statici nell’HTML
+     * Usate un template nell’html e clone() per l’ inserimento del messaggio da fare in JS
+     ************************************************************************************************/
+
+    // refs
+        
+    var inputSend = $('.bottom-bar-dx input'); //input
+    var iconChange = $('.bottom-bar-dx i:last-child'); // Icona
+
+    inputSend.focus( function() {
+        iconChange.removeClass('fas fa-microphone');
+        iconChange.addClass('fas fa-paper-plane');
+    });
+
+
+
+
+
+
+}); //<-- end Ready
