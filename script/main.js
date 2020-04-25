9660556fda7c5e495880e71b550ca9785980ef84
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-25 11:03:00 +0200

    add function js milestone 1

--- script/main.js
+++ script/main.js
@@ -17,49 +17,68 @@ $(document).ready(function () {
     var inputSend = $('.bottom-bar-dx input'); //input
     var iconChange = $('.bottom-bar-dx i:last-child'); // Icona
 
-
     // icon change on input focus
-    inputSend.focus( function() {
-        iconChange.removeClass('fas fa-microphone');
-        iconChange.addClass('fas fa-paper-plane');
+    inputSend.on(('focus blur'), function() {
+        iconChange.toggleClass('fas fa-microphone fas fa-paper-plane');
     });
 
     // send message with click icon
-    $('#app').on('click', '.bottom-bar-dx i:last-child', function() {
+    iconChange.click(function(){
+        smsSend(inputSend);
+    });
 
-        // clono il template
-        var messageTemplates = $('.templates .send').clone(); //refs li clone
-
-        var sendMessage = inputSend.val().trim();
-        console.log(sendMessage);
-
-        if (sendMessage !== '') {
-            // send message add to clone
-            messageTemplates.children().children('h5').text(sendMessage);
-            // append to html chat
-            $('.message-content ul').append(messageTemplates);
-            // reset input
-            inputSend.val('');
-        }   
-    } );
-
-
+    // send message with enter 
     inputSend.keyup(function (e) { 
-        // clono il template
-        var messageTemplates = $('.templates .send').clone(); //refs li clone
-
-        var sendMessage = inputSend.val().trim();
-        console.log(sendMessage);
-
-        if (sendMessage !== '' && e.which == 13 ) {
-             // send message add to clone
-             messageTemplates.children().children('h5').text(sendMessage);
-             // append to html chat
-             $('.message-content ul').append(messageTemplates);
-             // reset input
-             inputSend.val('');
+        if(e.which == 13) {
+            smsSend(inputSend);
         }
-
     });
    
 }); //<-- end Ready
+
+
+/**
+ * Function
+*/
+
+
+// sms send function
+function smsSend(input) {
+    //get text from the input
+    var inputMessage = input.val().trim();
+    //condition for empty field
+    if(inputMessage.length > 0) {
+        // clone template
+        var newMessage = $('.templates .message').clone(); //refs li clone
+        // add text at message
+        newMessage.children('h5').text(inputMessage);
+
+        //add dinamin time
+        var data = new Date();
+        var h = addZero(data.getHours());
+        var m = addZero(data.getMinutes());
+        var time = h + ':' + m;
+
+        newMessage.children('h6').text(time);
+
+        //add send style class
+        newMessage.addClass('send');
+
+        //add message at chat active
+        $('.message-content .chat.active ul').append(newMessage);
+
+        // reset input 
+        input.val('');
+    }
+
+};
+ 
+// add zero at time number < 10
+function addZero (number) {
+
+    if (number < 10) {
+        number = '0' + number;
+    }
+
+    return number;
+}
